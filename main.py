9b4b3e52d61c1a9f359d9fee2dc5075054d0095c
Multiversed <64979169+Multiversed@users.noreply.github.com>
2022-08-02 02:31:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -35,8 +35,8 @@ from datetime import datetime as clock
 
 while True:
 	time = clock.now().strftime('%I:%M%p')
-	print(f'({time}) bumping https://ogusers.com/showthread.php?tid={thread_ID_2bump}')
-	bump = post('https://ogusers.com/newreply.php', cookies={'mybbuser': mybbuser}, data=data)
+	print(f'({time}) bumping https://ogu.gg/showthread.php?tid={thread_ID_2bump}')
+	bump = post('https://ogu.gg/newreply.php', cookies={'mybbuser': mybbuser}, data=data)
 	if 'IM_UNDER_ATTACK_BOX' in bump.text:
 		print('^ bump failed!')
 		print('^^ captcha detected, evasion unsupported in prototype.')
